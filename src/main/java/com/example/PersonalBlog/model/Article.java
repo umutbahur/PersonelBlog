c6785114da114554af31ee62f724746fc88b74f6
umutbahur <87827469+umutbahur@users.noreply.github.com>
2025-01-24 23:48:13 +0300

    Project initialized.

---
+++ src/main/java/com/example/PersonalBlog/model/Article.java
@@ -0,0 +1,35 @@
+package com.example.PersonalBlog.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "article", schema = "blogwebsite")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDateTime publicationDate;
+    
+}
